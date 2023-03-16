28ec6f4665f55cda6ef2b073902d7a10c51a4c4c
Thiago Fabri de Oliveira <91853113+ThiagoFabri@users.noreply.github.com>
2023-03-16 16:56:02 -0300

    Update sla.py

--- sla.py
+++ sla.py
@@ -16,30 +16,27 @@ def tratar_data(data1,data2):
         data2=data2.replace(hour=17, minute=30)#obs: caso voce queira calcular a hora extra deverá implenmentar isso não é dificil faça isso dentro desse if antes de fazer o replace voce tera que criar uma nova variavel ok? não esqueça de deixar essa variavel = 0 antes do if e colocar no return
     return(data1,data2)
 
-def sla(data1,data2):#função do SLA
-    if data1>data2:
-        return 0
-    data1,data2=tratar_data(data1,data2)#tratar a data com a função: objetivo dela é colocar a data dentro do horario comercial
-    datas = pd.date_range(start=data1, end=data2, freq='d').to_series()#estou pegando todas as data que tem em diferença entre a data1 e data2(isso é um range)
+def sla(data1,data2):
+    data1,data2=tratar_data(data1,data2)
+    datas = pd.date_range(start=data1, end=data2, freq='d').to_series()
     horas=0
-    if data1.date() == data2.date():#caso a data seja igual fazer a subtração
+    if data1.date() == data2.date():
         horas=(data2 - data1).total_seconds() / 3600
     else:
         # Selecionar apenas as datas de segunda a sexta-feira
-        data_uteis=datas[datas.dt.weekday < 5]#dentro do range que criei quero tirar todas as datas sabado e domingo usando o weekday
-        if len(data_uteis)>=2:# se a quantidade de dias for maior que 1, ou seja, mais que um dias de diferença mutiplica vezes 8 esses dias
+        data_uteis=datas[datas.dt.weekday < 5]
+        if len(data_uteis)>=2:
             horas=(len(data_uteis)-1)*8
-        #importante divisão precisamos calcular quantas horas gastamos no primeiro dia
-        nova_data1 = data1.replace(hour=17, minute=30#fantoche para saber ate a ultima hora
+        nova_data1 = data1.replace(hour=17, minute=30)
 
         horas_inicial=( nova_data1-data1).total_seconds() / 3600
         if horas_inicial >=5:
-            horas_inicial-=1#caso a hora for maior ou igual a 5 fazer - 1 pois tenho que calcular o horario do almoço, isso é relativo dependendo do seu horario comercial
-        #importante divisão precisamos calcular quantas horas gastamos no ultimo dia
+            horas_inicial-=1
+        #data2 calculo
         nova_data2 = data2.replace(hour=8, minute=30)
 
         horas_final=(data2- nova_data2).total_seconds() / 3600
         if horas_final >=5:
             horas_final-=1
-        horas += horas_final + horas_inicial#somamos tudo que fizemos (calcular quantas horas gastamos no ultimo dia+calcular quantas horas gastamos no primeiro dia+(quantidade de dias for maior que 2*8))
-        return(horas)
+        horas += horas_final + horas_inicial
+    return (horas)
